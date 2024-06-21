20a4d667acb21770b691acd73536eb365a77a97b
Marcos <marcosrocha222@gmail.com>
2024-06-21 01:25:06 -0300

    motorista

--- src/Pages/HomeMotorista.js
+++ src/Pages/HomeMotorista.js
@@ -1,37 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import alunos from "../components/alunos"; // Importe a lista de alunos fictícios
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 20,
   },
+  logoutButton: {
+    padding: 10,
+    alignSelf: 'flex-end',
+    marginRight: 20,
+    marginTop: 20,
+  },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 20,
   },
   titulo: {
     fontSize: 24,
-    color: 'gray',
-    fontWeight: 'bold',
+    color: "gray",
+    fontWeight: "bold",
     marginBottom: 5,
   },
   subtitulo: {
     fontSize: 12,
-    color: 'black',
+    color: "black",
     marginBottom: 10,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   fotoPerfil: {
     width: 80,
     height: 80,
     borderRadius: 50,
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     right: 0,
     marginRight: 20,
@@ -42,13 +50,13 @@ const styles = StyleSheet.create({
   },
   tituloLista: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
-    color: 'black'
+    color: "black",
   },
   itemLista: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 10,
   },
   fotoAluno: {
@@ -61,49 +69,49 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   nomeAluno: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   status: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   statusIcon: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginRight: 10,
   },
-  SubtituloLista: {    
-    position: 'absolute',
+  SubtituloLista: {
+    position: "absolute",
     top: 40,
     right: 30,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   icone: {
     width: 20,
     height: 20,
-    
-    position: 'absolute',
+
+    position: "absolute",
     top: 0,
     right: 0,
-    left: 145 ,
+    left: 145,
   },
   icones: {
     width: 20,
     height: 20,
-    
-    position: 'absolute',
+
+    position: "absolute",
     top: 0,
     right: 0,
-    left: 170 ,
+    left: 170,
   },
-   historicoContainer: {
+  historicoContainer: {
     marginTop: 20,
   },
   historicoTitulo: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
-    color: 'black'
+    color: "black",
   },
   rota: {
     marginBottom: 5,
@@ -111,16 +119,14 @@ const styles = StyleSheet.create({
 });
 
 const HomeMotorista = ({ route, navigation }) => {
-
-  const [nomeMotorista, setNomeMotorista] = useState('');
+  const [nomeMotorista, setNomeMotorista] = useState("");
   const auth = getAuth();
   const db = getFirestore();
 
-  useEffect
-(() => {
+  useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
-        const docRef = doc(db, 'users', user.uid);
+        const docRef = doc(db, "users", user.uid);
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
           setNomeMotorista(docSnap.data().nome); // Supondo que o campo no Firestore seja 'nome'
@@ -130,30 +136,50 @@ const HomeMotorista = ({ route, navigation }) => {
     return unsubscribe; // Limpa o listener quando o componente desmonta
   }, []);
 
-  
+  const handleLogout = async () => {
+    try {
+      await signOut(auth); // Deslogar o usuário do Firebase
+      console.log("Logout realizado com sucesso!");
+      navigation.replace("Welcome"); // Navegar para a tela de boas-vindas
+    } catch (error) {
+      console.error("Erro ao fazer logout:", error);
+      Alert.alert("Erro", "Ocorreu um erro ao fazer logout.");
+    }
+  };
 
   const historicoRotas = [
-    {  imagem: require('./components/tela.png') },
-    {  imagem: require('./components/tela.png') },
-    {  imagem: require('./components/tela.png') },
+    { imagem: require("../img/tela.png") },
+    { imagem: require("../img/tela.png") },
+    { imagem: require("../img/tela.png") },
   ];
 
   return (
     <View style={styles.container}>
+      <View>
+        <TouchableOpacity
+          style={styles.logoutButton}
+          onPress={() => navigation.navigate("Welcome")}
+        >
+          <Icon name="sign-out" size={0} color="rgba(34, 0, 0, 0.533333)" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Icon name="sign-out" size={30} color="rgba(34, 0, 0, 0.533333)" />
+        </TouchableOpacity>
+      </View>
       <View style={styles.header}>
         <View>
           <Text style={styles.titulo}>Olá, {nomeMotorista}!</Text>
           <Text style={styles.subtitulo}>Veículo cadastrado: SLH2008</Text>
         </View>
         <Image
-          source={require('./components/iconTeste.png')} 
+          source={require("../img/iconTeste.png")}
           style={styles.fotoPerfil}
         />
       </View>
 
       <View style={styles.conteudo}>
         <Text style={styles.tituloLista}>Situação dos Alunos</Text>
-        <Text style={styles.SubtituloLista}>Ida  Volta</Text>
+        <Text style={styles.SubtituloLista}>Ida Volta</Text>
         <ScrollView>
           {alunos.map((aluno, index) => (
             <View key={index} style={styles.itemLista}>
@@ -164,44 +190,42 @@ const HomeMotorista = ({ route, navigation }) => {
                   <View style={styles.statusIcon}>
                     {aluno.ida ? (
                       <Image
-                        source={require('./components/iconVe.png')} 
+                        source={require("../img/iconVe.png")}
                         style={styles.icone}
                       />
                     ) : (
                       <Image
-                        source={require('./components/iconVe.png')} 
+                        source={require("../img/iconVe.png")}
                         style={styles.icone}
                       />
                     )}
-                    
                   </View>
                   <View style={styles.statusIcon}>
                     {aluno.volta ? (
                       <Image
-                        source={require('./components/iconX.png')} 
+                        source={require("../img/iconX.png")}
                         style={styles.icones}
                       />
                     ) : (
                       <Image
-                        source={require('./components/iconX.png')} 
+                        source={require("../img/iconX.png")}
                         style={styles.icones}
                       />
                     )}
-                    
                   </View>
                 </View>
               </View>
             </View>
           ))}
-           <View style={styles.historicoContainer}>
+          <View style={styles.historicoContainer}>
             <Text style={styles.historicoTitulo}>Histórico de Rotas</Text>
             {historicoRotas.map((rota, index) => (
               <View key={index} style={styles.rota}>
                 <Image source={rota.imagem} style={styles.rotaImagem} />
                 <Text style={styles.rotaTexto}>{rota.data}</Text>
               </View>
-          ))}
-        </View>
+            ))}
+          </View>
         </ScrollView>
       </View>
     </View>
---
+++ src/components/alunos.js
@@ -0,0 +1,49 @@
+const alunos = [
+    {
+      nome: 'Maria',
+      foto: require('../img/iconTeste.png'),
+      ida: true,
+      volta: true,
+    },
+    {
+      nome: 'João',
+      foto: require('../img/iconTeste.png'),
+      ida: false,
+      volta: false,
+    },
+    {
+      nome: 'Ana',
+      foto: require('../img/iconTeste.png'),
+      ida: true,
+      volta: true,
+    },
+    {
+      nome: 'Ana',
+      foto: require('../img/iconTeste.png'),
+      ida: false,
+      volta: false,
+    },
+    {
+      nome: 'Ana',
+      foto: require('../img/iconTeste.png'),
+      ida: false,
+      volta: false,
+    },
+    {
+      nome: 'Ana',
+      foto: require('../img/iconTeste.png'),
+      ida: false,
+      volta: false,
+    },
+    {
+      nome: 'Ana',
+      foto: require('../img/iconTeste.png'),
+      ida: false,
+      volta: false,
+    },
+   
+    // Adicione mais alunos conforme necessário
+  ];
+  
+  export default alunos;
+  
